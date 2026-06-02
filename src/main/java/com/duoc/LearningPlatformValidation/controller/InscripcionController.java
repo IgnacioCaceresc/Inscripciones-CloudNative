5f46c561265b1144aac9e9299c5f8013a6dad6af
IgnacioCaceresc <ignacioarturoc@gmail.com>
2026-06-01 21:09:14 -0400

    Agregar DTO con detalle de inscripciones

--- src/main/java/com/duoc/LearningPlatformValidation/controller/InscripcionController.java
+++ src/main/java/com/duoc/LearningPlatformValidation/controller/InscripcionController.java
@@ -1,37 +1,3 @@
-/*package com.duoc.LearningPlatformValidation.controller;
-
-import com.duoc.LearningPlatformValidation.model.Inscripcion;
-import com.duoc.LearningPlatformValidation.service.InscripcionService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import java.util.List;
-
-@RestController
-@RequestMapping("/api/inscripciones")
-public class InscripcionController {
-
-    @Autowired
-    private InscripcionService inscripcionService;
-
-    @PostMapping
-    public ResponseEntity<Inscripcion> inscribir(@RequestBody Inscripcion inscripcion) {
-        return new ResponseEntity<>(inscripcionService.registrarInscripcion(inscripcion), HttpStatus.CREATED);
-    }
-
-    @GetMapping("/curso/{cursoId}")
-    public ResponseEntity<List<Inscripcion>> listarPorCurso(@PathVariable Long cursoId) {
-        return ResponseEntity.ok(inscripcionService.listarPorCurso(cursoId));
-    }
-    
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> eliminar(@PathVariable Long id) {
-        inscripcionService.eliminarInscripcion(id);
-        return ResponseEntity.noContent().build();
-    }
-}*/
-
 package com.duoc.LearningPlatformValidation.controller;
 
 import com.duoc.LearningPlatformValidation.dto.InscripcionDTO;
